f6c74be77d9be2b638e5a537019daeff15e71cb6
Kevin Robinson <kevinr.zan@gmail.com>
2017-10-24 14:37:18 -0700

    windows hack sorry :(

--- src/client.py
+++ src/client.py
@@ -5,7 +5,8 @@ import socket
 def client(message):
     """."""
     infos = socket.getaddrinfo('127.0.0.1', 5001)
-    print(infos)
+    if infos[0][1] == 0:
+        infos = [(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP, '', ('127.0.0.1', 5001))]
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
@@ -19,3 +20,7 @@ def client(message):
         if len(part) < buffer_length:
             print(reply)
             break
+
+def lookat(sock = 5001):
+    infos = socket.getaddrinfo('127.0.0.1', sock)
+    print(infos)
