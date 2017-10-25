19e2454dc549c8cde9dc3c4ee449137ce9e7d2d9
Kevin Robinson <kevinr.zan@gmail.com>
2017-10-25 13:47:43 -0700

    small changes

--- src/server.py
+++ src/server.py
@@ -8,6 +8,7 @@ def server():
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
         address = ('127.0.0.1', 5001)
         server.bind(address)
+        full_message = u''
         while True:
             server.listen(1)
             conn, addr = server.accept()
@@ -18,16 +19,25 @@ def server():
                 part = conn.recv(buffer_length)
                 message += part
                 if b'|' in message:
-                    print(message.decode('utf8')[:-1])
-                    message = message.decode('utf8')
+                    message = message.decode('utf8')[:-1]
+                    print(message)
+                    full_message += message
                     break
             # import pdb; pdb.set_trace()
-            conn.sendall(message.encode('utf8'))
+            conn.sendall(response_ok())
             conn.close()
 
-    except Exception:
-        print('***EXCEPTION***')
+    except KeyboardInterrupt:
+
         # import pdb; pdb.set_trace()
         conn.close()
         server.close()
         raise
+
+def response_ok():
+    '''.'''
+    return b'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nThis is a complete response.\r\n|'
+
+def response_error():
+    '''.'''
+    return b'HTTP/1.1 500 Internal Server Error\r\n\r\n|'
