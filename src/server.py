309c6fb09e751ad14cab427e1c42b017a04fc5ab
jensjoh01 <johnnyj2439@gmail.com>
2017-10-27 15:36:57 -0700

    made port numbers the same

--- src/server.py
+++ src/server.py
@@ -9,7 +9,7 @@ def server():
     try:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM,
                                socket.IPPROTO_TCP)
-        address = ('127.0.0.1', 5000)
+        address = ('127.0.0.1', 5001)
         server.bind(address)
         response_message = u''
         while True:
