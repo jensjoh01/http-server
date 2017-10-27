6b5d361120df46164148ffe0dadb8d21662a1c05
Kevin Robinson <kevinr.zan@gmail.com>
2017-10-27 14:59:15 -0700

    docstrings - minor

--- src/test_servers.py
+++ src/test_servers.py
@@ -1,4 +1,4 @@
-"""."""
+"""test for the client"""
 import pytest
 # test that the message sent to the server
 # is the same as the message received
@@ -15,6 +15,7 @@ import pytest
 #     assert message == client(message)
 
 def test_response_ok_from_server():
+    '''response from server should be the standard OK response'''
     from client import client
     from server import response_ok
     assert client('message') == response_ok()[:-1].decode('utf8')
